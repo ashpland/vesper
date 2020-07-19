43c320d10780d50384e2b48d0f670c55a2d724bd
Andrew Shopland <hi@ashp.land>
2020-07-18 18:40:13 -0700

    Undelete views.cljs

---
+++ src/vesper/views.cljs
@@ -0,0 +1,19 @@
+(ns vesper.views
+  (:require
+   [re-frame.core :as re-frame]
+   [vesper.chat :as chat]
+   [vesper.recepient :as recepient]
+   ))
+
+(defn main-panel []
+  []
+  (let [*recepient (re-frame/subscribe [::recepient/current-recepient])]
+    (fn []
+    [:div
+     [recepient/SelectRecepient]
+     (when @*recepient
+       [:<>
+       [:hr]
+       [:h3 "Dear " @*recepient]
+       [chat/ChatView]])
+     ])))
